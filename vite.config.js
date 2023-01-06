835b204a7a888f4c935c79a9c6ccf6bc7f749412
Shevchenko Daniil <72701362+Vailz@users.noreply.github.com>
2023-01-06 18:09:09 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 export default defineConfig({
     plugins: [
         laravel([
-            'node resources/js/app.js',
+            'resources/js/app.js',
         ]),
     ],
     resolve: {
